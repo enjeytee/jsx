c2beeb21b70e391a320dc93ad6c68aa8fe0172d5
enjeytee <enjeytee@gmail.com>
2022-09-02 18:24:52 +0800

    af

---
+++ index.js
@@ -0,0 +1,15 @@
+const navBar = (
+    <nav>
+        <h1>My Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    navBar,
+    document.querySelector("#root")
+)
